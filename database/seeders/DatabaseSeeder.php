1ea92d26f71eac1e2670b877e1e54b9583382467
PoofDevelopment <petervgarderen@outlook.com>
2023-01-05 22:21:12 +0100

    Added TrainTrack seeder to DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             TrainSeeder::class,
             CarriageSeeder::class,
+            TrainTrackSeeder::class,
         ]);
     }
 }
