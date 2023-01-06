b050388e2c1d8a785e4dad7907b5eb0a68e778ab
PetervGarderen <37441410+loxiuras@users.noreply.github.com>
2023-01-06 13:44:58 +0100

    Added TrainRide model

---
+++ app/Models/TrainRide.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TrainRide extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'number',
+        'train_track_id',
+        'direction',
+        'train_id',
+        'start_at',
+        'end_at',
+    ];
+
+    protected $hidden = [];
+
+    protected $casts = [
+        'start_at' => 'datetime',
+        'end_at' => 'datetime',
+    ];
+
+    ##### RELATIONS #####
+
+    public function trainTrack(): belongsTo
+    {
+        return $this->belongsTo(TrainTrack::class);
+    }
+
+    public function train(): belongsTo
+    {
+        return $this->belongsTo(Train::class);
+    }
+}
