3b5a20abc86318cc13ec54aaa81bf8b0a7119b26
PetervGarderen <37441410+loxiuras@users.noreply.github.com>
2023-01-06 16:01:41 +0100

    Adjusted Train ride seeder

--- database/seeders/TrainRideSeeder.php
+++ database/seeders/TrainRideSeeder.php
@@ -13,19 +13,19 @@ class TrainRideSeeder extends Seeder
 
     public function run(): void
     {
-        $this->createTrainRide(1, 1);
-        $this->createTrainRide(2, 2);
-        $this->createTrainRide(3, 3);
+        $this->createTrainRide(1, 1, [1,2,3,4,5]);
+        $this->createTrainRide(2, 2, [6,7,8,9,10]);
+        $this->createTrainRide(3, 3, [36,37,38,39,40]);
     }
 
-    private function createTrainRide(int $trainId, int $trainTrackId): void
+    private function createTrainRide(int $trainId, int $trainTrackId, array $carriageIds = null): void
     {
         $trainTrack = TrainTrack::find($trainTrackId);
 
         $stations = $trainTrack->trainTrackStations;
         $durationInSeconds = (count($stations) - 1) * $this->secondsPerSubStation;
 
-        TrainRide::updateOrCreate([
+        $trainRide = TrainRide::updateOrCreate([
             'number' => rand(100000000, 999999999),
             'train_id' => $trainId,
             'train_track_id' => $trainTrackId,
@@ -33,5 +33,13 @@ class TrainRideSeeder extends Seeder
             'start_at' => now(),
             'end_at' => now()->addSeconds($durationInSeconds),
         ]);
+
+        if (!is_null($carriageIds)) {
+            foreach ($carriageIds as $carriageId) {
+                $trainRide->carriages()->updateOrCreate([
+                    'carriage_id' => $carriageId,
+                ], []);
+            }
+        }
     }
 }
