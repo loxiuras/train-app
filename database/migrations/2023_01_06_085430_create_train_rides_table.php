e01cf724f06cf5fc472cdbf2c7fdf15fde47805c
PetervGarderen <37441410+loxiuras@users.noreply.github.com>
2023-01-06 13:44:48 +0100

    Added TrainRide migration

---
+++ database/migrations/2023_01_06_085430_create_train_rides_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('train_rides', function (Blueprint $table) {
+            $table->id();
+            $table->string('number');
+            $table->foreignId('train_track_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->string('direction')->default('forwards');
+            $table->foreignId('train_id')->constrained()->cascadeOnUpdate()->cascadeOnDelete();
+            $table->dateTime('start_at')->nullable();
+            $table->dateTime('end_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('train_rides');
+    }
+};
